722702f00f3f3c6a4533bba49434b80654a5b897
CyberMehul <mehulrathod9254@gmail.com>
2025-11-06 15:26:02 +0530

    fix: cors issue

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const { initialDatabase } = require("./db/db.connect");
 const cors = require("cors");
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://localhost:5173"],
+  origin: "*",
   credentials: true,
 };
 
@@ -17,7 +17,6 @@ initialDatabase();
 // MIDDLEWARES
 app.use(express.json());
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
 
 // ROUTES :
 // [ 1 ] AGENTS API :
